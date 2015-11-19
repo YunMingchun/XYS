6a5e0d7ac344f4ac4cd4efb84cea8f6bf0fdf54c
ymc <19985321976@qq.com>
2015-11-19 02:31:19 -0800

    XYAdmin CRUD -- 20151119

--- XYAdmin/api/admin/partTimeJob.js
+++ XYAdmin/api/admin/partTimeJob.js
@@ -7,8 +7,8 @@ var router = express.Router();
 var mongodb = require('../../models/db');
 var ObjectId = require('mongodb').ObjectID;
 
-/* GET Part Time Job List page. */
 router.post('/add.json', function(req, res, next) {
+    var userId = req.body.userId;
     mongodb.open(function (err, db) {
         db.collection('partTimeJobs', function (err, collection) {
             collection.insert(req.body, function (err, result) {
@@ -16,6 +16,70 @@ router.post('/add.json', function(req, res, next) {
                 res.json({
                     status: 0,
                     id: result.insertedIds[0]
+                });
+            });
+        });
+    });
+});
+
+router.get('/list.json', function(req, res ,next) {
+    var userId = req.query.userId;
+    mongodb.open(function(err, db){
+        db.collection('partTimeJobs', function(err, collection){
+            collection.find().toArray(function(err, docs){
+                mongodb.close();
+                res.json({
+                    status: 0,
+                    list: docs
+                });
+            });
+        });
+    });
+});
+
+router.get('/item.json', function(req, res ,next) {
+    var userId = req.query.userId;
+    mongodb.open(function(err, db){
+        db.collection('partTimeJobs', function(err, collection){
+            collection.findOne({
+                _id: ObjectId(req.query.itemId)
+            }, function(err, item){
+                mongodb.close();
+                res.json({
+                    status: 0,
+                    item: item
+                });
+            });
+        });
+    });
+});
+
+router.post('/update.json', function(req, res ,next) {
+    var userId = req.body.userId;
+    mongodb.open(function(err, db){
+        db.collection('partTimeJobs', function(err, collection){
+            collection.findOneAndUpdate({
+                _id: ObjectId(req.body.itemId)
+            }, req.body, function(err, result){
+                mongodb.close();
+                res.json({
+                    status: 0
+                });
+            });
+        });
+    });
+});
+
+router.post('/delete.json', function(req, res ,next) {
+    var userId = req.body.userId;
+    mongodb.open(function(err, db){
+        db.collection('partTimeJobs', function(err, collection){
+            collection.findOneAndDelete({
+                _id: ObjectId(req.body.itemId)
+            }, function(err, result){
+                mongodb.close();
+                res.json({
+                    status: 0
                 });
             });
         });
--- XYAdmin/public/js/controller/admin/partTimeJob.js
+++ XYAdmin/public/js/controller/admin/partTimeJob.js
@@ -6,9 +6,36 @@ myApp.controller('frameCtrl', function($scope, $http, $location) {
 });
 
 myApp.controller('listCtrl', function($scope, $http, $location) {
+    $scope.list = function(){
+        $http.get('/api/admin/part-time-job/list.json', {
+            params: {
+                userId: 1
+            }
+        }).success(function(resp){
+            if(resp.status == 0){
+                $scope.items = resp.list;
+            }
+        });
+    }
     $scope.addItem = function(){
         $location.url('/editor?mode=0');
     }
+    $scope.editItem = function (id) {
+        $location.url('/editor?mode=1&itemId='+id);
+    }
+    $scope.deleteItem = function (id) {
+        $http.post('/api/admin/part-time-job/delete.json', {
+            userId: 1,
+            itemId: id
+        }).success(function(resp){
+            if(resp.status == 0){
+                $scope.list();
+            }
+        });
+    }
+
+    /* Play */
+    $scope.list();
 });
 
 myApp.controller('editorCtrl', function($scope, $http, $location, $routeParams) {
@@ -47,7 +74,53 @@ myApp.controller('editorCtrl', function($scope, $http, $location, $routeParams) 
         }
     }
     else if(mode == 1){
+        var itemId = $routeParams.itemId;
 
+        $scope.getItem = function(){
+            $http.get('/api/admin/part-time-job/item.json', {
+                params: {
+                    userId: 1,
+                    itemId: itemId
+                }
+            }).success(function(resp){
+                if(resp.status == 0){
+                    $scope.publisher = resp.item.publisher;
+                    $scope.area = resp.item.area;
+                    $scope.payment = resp.item.payment;
+                    $scope.count = resp.item.count;
+                    $scope.experience = resp.item.experience;
+                    $scope.degree = resp.item.degree;
+                    $scope.detail = resp.item.detail;
+                    $scope.promise = resp.item.promise;
+                    $scope.address = resp.item.address;
+                }
+            });
+        }
+        $scope.submit = function(){
+            $http.post('/api/admin/part-time-job/update.json', {
+                userId: 1,
+                itemId: itemId,
+                publisher: $scope.publisher,
+                area: $scope.area,
+                payment: $scope.payment,
+                count: $scope.count,
+                experience: $scope.experience,
+                degree: $scope.degree,
+                detail: $scope.detail,
+                promise: $scope.promise,
+                address: $scope.address
+            }).success(function(resp){
+                if(resp.status == 0){
+                    $location.url('/');
+                }
+            });
+        }
+        $scope.cancel = function(){
+            $location.url('/');
+        }
+
+        /* Play */
+        $scope.getItem();
     }
 });
 
